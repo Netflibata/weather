2b0ba0275300a5eacf71d6b1295ebdfbd36bc6b8
Netflibata <114660306+Netflibata@users.noreply.github.com>
2024-07-02 14:40:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def get_tianixnweather():
     result = tianapi.read()
     data = result.decode('utf-8')
     dict_data = json.loads(data)
-    content = dict_data["result"]["alarmlist"][4]"content"]
+    content = dict_data["result"]["alarmlist"][4]["content"]
     return content
 
 
