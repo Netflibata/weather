3bd84b65112b9d7521c9f016156ea7973fce8a36
Netflibata <114660306+Netflibata@users.noreply.github.com>
2024-07-02 13:49:42 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en, tianxinwe):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -184,6 +184,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
             },
             "note_ch": {
                 "value": note_ch,
+                "color": get_color()
+            },
+            "tianxinwe": {
+                "value": tianxinwe,
                 "color": get_color()
             }
         }
